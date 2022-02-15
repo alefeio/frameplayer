3c398dc76b8fe922b528014abe1908c40f19def7
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2022-02-14 22:50:02 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ import { images } from "./components/Images";
 import './styles.scss'
 
 export function App() {
-  const { state, setState } = useContext(ThumbContext);
-
-  function alterImgs(imgs) {
-    setState({ ...state, imgs })
-  }
-
-  useEffect(() => {
-    alterImgs(images)
-  }, [])
-
   return (
     <div className="App">
       <FramePlayer frames={images} fps={0.2} />
